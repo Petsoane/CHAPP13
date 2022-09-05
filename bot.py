af31e1fb3a1aaaeaf210f079230ec77ad59e43a7
Lebogang Petsoane <lpetsoan@student.wethinkcode.co.za>
2022-09-05 17:55:59 +0200

    ginal

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import logging
 import telegrampy
-import telegrampy.ext import commands
+from telegrampy.ext import commands
 
 
 logging.basicConfig(level=logging.INFO, format="(%(asctime)s) %(levelname)s %(message)s", datefmt="%m/%d/%y - %H:%M:%S %Z")
 logger = logging.getLogger("telegrampy")
 
-bot = commands.Bot("token here")
+bot = commands.Bot("5750214131:AAE_lVnxeI690D4qweeUNHShgXMCTY4-Fxc")
 
 @bot.command()
 async def hi(ctx):
