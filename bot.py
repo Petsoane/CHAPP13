2a40ec24998a4ce976f26f6ead0f8a493de62dca
Lebogang Petsoane <lpetsoan@student.wethinkcode.co.za>
2022-09-05 18:05:47 +0200

    Changin the api handler

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('TOKEN').build()
+    application = ApplicationBuilder().token('5750214131:AAE_lVnxeI690D4qweeUNHShgXMCTY4-Fxc').build()
 
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
