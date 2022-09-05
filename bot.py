c61c569f45ed7e1724f84b61257be588dc3bce02
Lebogang Petsoane <lpetsoan@student.wethinkcode.co.za>
2022-09-05 18:01:44 +0200

    changel

--- bot.py
+++ bot.py
@@ -1,15 +1,19 @@
 import logging
-import telegrampy
-from telegrampy.ext import commands
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
-logging.basicConfig(level=logging.INFO, format="(%(asctime)s) %(levelname)s %(message)s", datefmt="%m/%d/%y - %H:%M:%S %Z")
-logger = logging.getLogger("telegrampy")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-bot = commands.Bot("5750214131:AAE_lVnxeI690D4qweeUNHShgXMCTY4-Fxc")
+if __name__ == '__main__':
+    application = ApplicationBuilder().token('TOKEN').build()
 
-@bot.command()
-async def hi(ctx):
-    await ctx.send("Hello")
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
 
-bot.run()
+    application.run_polling()
