9b08017596b778ddbf9b7b95611dac12304ac165
Lebogang Petsoane <lpetsoan@student.wethinkcode.co.za>
2022-09-05 15:45:59 +0200

    fixed some errors

--- app2.py
+++ app2.py
@@ -80,7 +80,7 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.bot.start_webhook(listen="0.0.0.0",
+    updater.start_webhook(listen="0.0.0.0",
                             port=PORT,
                             url_path="5750214131:AAE_lVnxeI690D4qweeUNHShgXMCTY4-Fxc")
     # updater.start_polling()
