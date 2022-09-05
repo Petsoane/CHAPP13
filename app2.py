146381b597d0e123eee2eff8724824f125b18262
Lebogang Petsoane <lpetsoan@student.wethinkcode.co.za>
2022-09-05 15:57:13 +0200

    added

--- app2.py
+++ app2.py
@@ -28,7 +28,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 PORT = int(os.environ.get('PORT','8443'))
-
+PORT = 8443
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
